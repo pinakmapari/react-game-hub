03fb09383d147c8656759f855441e9a78ea15ba3
Pinak Mapari <pinakmapari@gmail.com>
2024-02-21 00:13:55 +0530

    show loading skeletons

--- src/components/game-grid.tsx
+++ src/components/game-grid.tsx
@@ -1,13 +1,22 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames, { Game } from "../hooks/useGames";
 import GameCard from "./game-card";
+import GameCardSkeleton from "./game-card-skeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
+
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding='10px'>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding="10px"
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game: Game) => (
           <GameCard key={game.id} game={game}></GameCard>
         ))}
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "f448aa4535b245489dd9fbffd842e6c0",
+    key: "a89c455621394a6cb01a5b4ff590bb4f",
   },
 });
